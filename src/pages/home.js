f3f8c3a6409e4c0c5c311af4eeedc18de55b1000
Alix17 <wildflowerinabox@gmail.com>
2022-01-26 12:34:16 -0800

    initial file structure

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import { ???? } from "react-bootstrap";
+
+const myStyle = {
+  color: "#403F48",
+};
+
+const Home = () => {
+  return (
+    <div style={myStyle}>
+      <div className="Row py-3"></div>
+      <div>Hello World</div>
+    </div>
+  );
+};
+
+export default Home;
