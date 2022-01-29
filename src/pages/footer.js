bee0c6d24b05e8b48f3dd4bae65c7c61ac08c86b
Alix17 <wildflowerinabox@gmail.com>
2022-01-28 16:04:19 -0800

    added footer.js

---
+++ src/pages/footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+//import { ???? } from "react-bootstrap";
+
+
+
+const Home = () => {
+  return (
+    <!-- End of Portfolio Section -->	<!-- Client Section -->
+    <section id="client" class="overlay bg-fixed" style="background-image: url(img/bg.jpg);">
+        <div class="container">
+            <div class="section-content" data-aos="fade-up">
+                <div class="row ">
+                    <div class="col-md-12">
+                        <!-- Section Title -->
+                        <div class="title-wrap mb-5"><h2>My background</h2>
+                            <h5>My background is in horticulture, I spent the last 10 years managing at Sloat Garden Center. Plants
+                                have been a hobby of mine for a long time
+                                but I needed a new challenge and I would like to turn gardening back into a hobby and pursue a different
+                                line of work. I
+                                figured that if I could learn the latin names for most species of
+                                plants in California, I can learn how to code. This class is definitely a challenge for me, I'm going to be
+                                honest and say I'm struggling through this month. I've
+                                never even looked at code before the pre-work but I know with time and repetition I will get this.
+                                My first vegetable garden was the same way, I didn't give up and now I'm a great gardener, I hope to apply
+                                that mentality to this endeavor.</h5>
+                        </div>
+    
+                    </div>
+                </div>
+            </div>
+    </section>
+    <!-- End of Client Section -->	<!-- Reservation Section -->
+    <section id="reservation" class="bg-white section-content">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 offset-lg-1 mb-5 mb-lg-0" data-aos="fade-right">
+                    <div class="bg-white p-5 shadow">
+                        <div class="heading-section text-center">
+                            <h2 class="mb-4">
+                                Alix Friedman
+                            </h2>
+                        </div>
+    
+                    </div>
+                </div>
+                <div class="col-lg-4 offset-lg-1" data-aos="fade-left">
+                    <h2 class="mb-4">
+                        Contact:
+                    </h2>
+                    <ul class="list-inline py-2">
+                        <li class="list-inline-item text-center">
+                            <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Phone:
+                            (925)997-8067<br>
+                            <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>
+                            Email:
+                            Wildflowerinabox@gmail.com<br>
+                            <i class="fa fa-desktop fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i><a
+                                href="https://github.com/Alix1713">My GitHub</a><br>
+                            <i class="fa fa-users fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i><a
+                                href="https://www.linkedin.com/in/alix1713/">My Linked In</a><br>
+                            <i class="fa fa-book fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i><a
+                                href="Alix_Friedman_Resume.PDF">My Resume</a><br>
+                    </a>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    <!-- End of Reservation Section -->	</div>
+    </footer>	<!-- External JS -->
+    
+    );
+};
+
+export default footer;
